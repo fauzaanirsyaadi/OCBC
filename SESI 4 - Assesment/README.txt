5257dbed9ea7b5188e14d0fdff92dfb63b0de6d5
fauzaanirsyaadi <fauzaan.irsyaadi@gmail.com>
2021-10-12 17:07:43 +0700

    [ADD] adding sesi 4 assesment

---
+++ SESI 4 - Assesment/README.txt
@@ -0,0 +1,10 @@
+link repository : 
+1. git init
+2. git clone
+
+
+teknik penggunaan adalah sebagai berikut :
+1. masuk ke direktori number dari soal yang diinginkan (jika menggunakan terminala gunakan cd -nama direktory yang diinginkan).
+2. gunakan syntax - dotnet run - untuk menjalankan code
+3. gunakan sytac - cd .. - untuk keluar satu langkah dari direktory
+
---
+++ SESI 4 - Assesment/number6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace number6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1, rev;
+            Console.Write("enter your text : ");
+            string1 = Console.ReadLine();
+            char[] ch = string1.ToCharArray();
+            Array.Reverse(ch);
+            rev = new string(ch);
+            bool b = string1.Equals(rev, StringComparison.OrdinalIgnoreCase);
+            if (b == true) {
+                Console.WriteLine("" + string1 + " is a Palindrome!");
+            } else {
+                Console.WriteLine(" " + string1 + " is not a Palindrome!");
+            }
+            Console.Read(); 
+            }
+    }
+}
