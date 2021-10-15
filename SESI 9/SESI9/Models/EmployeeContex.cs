2034845a7eecb34721466ba1edaca301b70530b9
fauzaanirsyaadi <fauzaan.irsyaadi@gmail.com>
2021-10-15 13:04:17 +0700

    [add] hari ke 10 pagi

---
+++ SESI 9/SESI9/Models/EmployeeContex.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace SESI9.Models
+{
+    public class EmployeeContex
+    {
+        public string Conn2
+    }
+}
---
+++ SESI 9/SESI9/Models/EmployeeItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SESI9.Models
+{
+    public class EmployeeItem
+    {
+        private Models.EmployeeContect contect;
+
+        public int id { get; set; }
+        public string nama { get; set; }
+        public string JenisKelamin( get; set;)
+        public string alamat { get; set; }
+    }
+}
