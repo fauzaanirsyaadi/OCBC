a29c17ff08ead251108e65386ca76515d8b75f14
fauzaanirsyaadi <fauzaan.irsyaadi@gmail.com>
2021-10-17 00:14:24 +0700

    [add] tambah hari 5

---
+++ SESI 7/LoginSystem/LoginSystem/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginSystem
+{
+    
+    public partial class Form1 : Form
+    {
+        public static string nama = "";
+        public static string username = "";
+
+        Config db = new Config();
+
+        public Form1()
+        {
+            InitializeComponent();
+            db.connect("userdata");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            register register = new register();
+            register.Show();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            db.ExecuteSelect("SELECT * FROM user_info where username='" + textBox1.Text + "' and password ='" + textBox2.Text + "'");
+
+            if (db.Count() == 1)
+            {
+                nama = textBox1.Text;
+                username = textBox2.Text;
+                MessageBox.Show("Success You Will Login as " + db.Results(0, "names"));
+                FormData frm = new FormData();
+                frm.Show();
+                this.Hide();
+
+            } else
+            {
+                MessageBox.Show("Wrong username and password combination");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ SESI 7/LoginSystem/LoginSystem/FormData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginSystem
+{
+    public partial class FormData : Form
+    {
+        Config db = new Config();
+        public FormData()
+        {
+            InitializeComponent();
+            db.connect("userdata");
+
+            textBox1.Text = Form1.nama;
+            textBox2.Text = Form1.username;
+        }
+
+        private void FormData_Load(object sender, EventArgs e)
+        {
+            db.ExecuteSelect("SELECT * FROM data_staff");
+            
+           //ini cara sambung ke grid gmn
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            db.ExecuteSelect("SELECT * FROM data_staff where names="+textBox3.Text);
+
+        }
+    }
+}
