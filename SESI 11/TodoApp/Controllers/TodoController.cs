170c37a5771fc16a5ce0a908955bf2a4591cd69c
fauzaanirsyaadi <fauzaan.irsyaadi@gmail.com>
2021-10-18 14:46:37 +0700

    [add] sesi 11

---
+++ SESI 11/TodoApp/Controllers/TodoController.cs
@@ -0,0 +1,105 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Data;
+using TodoApp.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.Json;
+using Swashbuckle.AspNetCore;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace TodoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class TodoController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public TodoController(ApiDbContext context){
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetItems(){
+            var items = await _context.Items.ToListAsync();
+            return Ok(items);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateItem(ItemData data){
+            if(ModelState.IsValid){
+                await _context.Items.AddAsync(data);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetItem", new {data.id}, data);
+            }
+            return new JsonResult("Something went wrong!") {StatusCode = 500};
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItemById(int id){
+            var item = await _context.Items.FirstOrDefaultAsync(x => x.id == id);
+
+            if(item == null){
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateItem(int id, ItemData item){
+            if(id != item.id){
+                return BadRequest();
+            }
+
+            var existItem = await _context.Items.FirstOrDefaultAsync(x => x.id == id);
+
+            if(existItem == null){
+                return NotFound();
+            }
+
+            existItem.title = item.title;
+            existItem.description = item.description;
+            existItem.done = item.done;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItem(int id){
+            var existItem = await _context.Items.FirstOrDefaultAsync(x => x.id == id);
+
+            if(existItem == null){
+                return NotFound();
+            }
+
+            _context.Items.Remove(existItem);
+            await _context.SaveChangesAsync();
+
+            return Ok(existItem);
+        }
+    }
+}
---
+++ SESI 11/TodoApp/Data/ApiDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.SqlServer;
+//using Microsoft.EntityFrameworkCore.Tools;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace TodoApp.Data
+{
+    public class ApiDbContext : IdentityDbContext{
+        public virtual DbSet<ItemData> Items {get; set;}
+         public virtual DbSet<RefreshToken> RefreshTokens { get; set;}
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) 
+          : base(options)
+          {
+
+        }
+    }
+    
+}
